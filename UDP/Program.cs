6aeb416855c6efbfa2ba21f25d2eb2240bc02241
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-12-03 06:30:53 +0700

    add UDP program entry point with server listener and user input handling

---
+++ UDP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using UDP.Server;
+
+// Client
+// using var udpClient = new UdpClient("192.168.84.128", 1211);
+// var client = new Client(udpClient);
+
+// while (true)
+// {
+//   var message = GetStringFromUser();
+//   client.Send(message);
+// }
+
+// Server
+
+using var udpListener = new UdpClient(1211);
+
+var server = new Server(udpListener);
+_ = server.HandleMultipleResponses();
+
+while (true)
+{
+  GetStringFromUser();
+}
+
+static string GetStringFromUser() => Console.ReadLine()!;
