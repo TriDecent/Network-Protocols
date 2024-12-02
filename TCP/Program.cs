f0a5d3730ae19f655aff12cf17d71f4d96aef9fb
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-12-03 01:45:57 +0700

    add TCP program entry point with server initialization and client handling structure

---
+++ TCP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using TCP.Server;
+using TCP.Client;
+
+// Client
+// using var tcpClient = new TcpClient("192.168.84.128", 1211);
+// var client = new Client(tcpClient);
+
+// while (true)
+// {
+//   var command = GetStringFromUser();
+//   client.SendMessage(command);
+// }
+
+// Server
+using var listener = new TcpListener(IPAddress.Any, 1211);
+var server = new Server(listener);
+server.Start();
+
+_ = server.HandleMultipleClientConnectionsAsync();
+
+while (true)
+{
+  GetStringFromUser();
+}
+
+static string GetStringFromUser() => Console.ReadLine()!;
