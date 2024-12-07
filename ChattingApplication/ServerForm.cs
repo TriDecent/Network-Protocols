ea0ac153a26ca73196d4c2e433131dc8702e822e
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-12-08 03:44:53 +0700

    add ServerForm enhancements with UI components and event handling for server state and message reception

--- ChattingApplication/ServerForm.cs
+++ ChattingApplication/ServerForm.cs
@@ -1,20 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Net;
+using System.Net.Sockets;
+using ChattingApplication.Enums;
+using ChattingApplication.Events;
+using ChattingApplication.Utils;
 
 namespace ChattingApplication
 {
   public partial class ServerForm : Form
   {
+    private readonly Button _startServerButton, _stopServerButton;
+    private readonly Button _sendMessageButton;
+    private readonly Button _attachButton, _detachButton;
+    private readonly Label _stateLabel;
+    private readonly RichTextBox _chatDisplayArea;
+
+    private readonly ChatMessageRenderer _chatRenderer;
+
+    private readonly Server _server;
+
     public ServerForm()
     {
       InitializeComponent();
+
+      _startServerButton = btnStart;
+      _stopServerButton = btnStop;
+      _sendMessageButton = btnSend;
+      _stateLabel = lblState;
+      _attachButton = btnAttach;
+      _detachButton = btnDetach;
+      _chatDisplayArea = rtbDialogArea;
+
+      _chatRenderer = new ChatMessageRenderer(_chatDisplayArea);
+
+      var tcpListener = new TcpListener(IPAddress.Any, 1211);
+      _server = new Server(tcpListener);
+
+      _server.MessageReceivedEventHandler += OnMessageReceived;
+      _server.StateChangedEventHandler += OnStateChanged;
     }
+
+    private void OnStateChanged(object? sender, StateChangedEventArgs e)
+    {
+      var serverState = e.ServerState;
+
+      _stateLabel.Text = $"State: {serverState}";
+
+      _startServerButton.Enabled = serverState != ServerState.Starting
+        && serverState != ServerState.ShuttingDown;
+
+      _stopServerButton.Enabled = _startServerButton.Enabled;
+
+      _startServerButton.Visible = serverState != ServerState.Listening;
+      _stopServerButton.Visible = !_startServerButton.Visible;
+    }
+
+    private void OnMessageReceived(object? sender, MessageReceivedEventArgs e)
+    {
+      if (e.Type == MessageType.Image)
+      {
+        var bytes = e.Content as byte[] ?? [];
+        _chatRenderer.DisplayImage("Client", bytes.BytesToImage(), false);
+        return;
+      }
+
+      var message = e.Content as string ?? "";
+      _chatRenderer.DisplayMessage("Client", message, false);
+    }
+
+    private async void BtnStart_Click(object sender, EventArgs e)
+    {
+      _server.Start();
+      await _server.HandleMultipleConnections();
+    }
+
+    private void BtnClose_Click(object sender, EventArgs e) 
+      => _server.Stop();
   }
 }
