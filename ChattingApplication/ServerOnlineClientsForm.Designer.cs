277181e07f49f4d06c5ce04ec2f434e2c703e3a4
TriDecent <116976929+TriDecent@users.noreply.github.com>
2025-01-27 03:53:19 +0700

    add ServerOnlineClientsForm displaying online clients to DM them

---
+++ ChattingApplication/ServerOnlineClientsForm.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace ChattingApplication
+{
+  partial class ServerOnlineClientsForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      lvOnlineClients = new ListView();
+      SuspendLayout();
+      // 
+      // lvOnlineUsers
+      // 
+      lvOnlineClients.Location = new Point(12, 12);
+      lvOnlineClients.Name = "lvOnlineUsers";
+      lvOnlineClients.Size = new Size(389, 308);
+      lvOnlineClients.TabIndex = 0;
+      lvOnlineClients.UseCompatibleStateImageBehavior = false;
+      // 
+      // ServerOnlineUsersForm
+      // 
+      AutoScaleDimensions = new SizeF(7F, 15F);
+      AutoScaleMode = AutoScaleMode.Font;
+      ClientSize = new Size(413, 332);
+      Controls.Add(lvOnlineClients);
+      Name = "ServerOnlineUsersForm";
+      Text = "Online Users";
+      ResumeLayout(false);
+    }
+
+    #endregion
+
+    private ListView lvOnlineClients;
+  }
+}
---
+++ ChattingApplication/ServerOnlineClientsForm.cs
@@ -0,0 +1,88 @@
+﻿using ChattingApplication.Common.Events;
+using ChattingApplication.Core.Interfaces;
+using ChattingApplication.Core.Models;
+
+namespace ChattingApplication
+{
+  public partial class ServerOnlineClientsForm : Form
+  {
+    private readonly ListView _lvOnlineClients;
+
+    public ServerOnlineClientsForm(IServer server)
+    {
+      InitializeComponent();
+      _lvOnlineClients = lvOnlineClients;
+
+      _lvOnlineClients.View = View.Details;
+      _lvOnlineClients.FullRowSelect = true;
+      _lvOnlineClients.MultiSelect = false;
+      _lvOnlineClients.Columns.Add("Online Clients", -2, HorizontalAlignment.Left);
+
+      _lvOnlineClients.DoubleClick += OnClientDoubleClick;
+
+
+      var clientsInfo = server.ClientsInfo;
+      DisplayClientsInfo(clientsInfo);
+
+      server.ClientConnectedEventHandler += OnConnectedClient;
+      server.ClientDisconnectedEventHandler += OnDisconnectedClient;
+    }
+
+    private void DisplayClientsInfo(IEnumerable<ClientSessionInfo> clientsInfo)
+    {
+      _lvOnlineClients.Items.Clear();
+      foreach (var clientInfo in clientsInfo)
+      {
+        var item = new ListViewItem(clientInfo.ToString())
+        {
+          Tag = clientInfo
+        };
+        _lvOnlineClients.Items.Add(item);
+      }
+    }
+
+    private void OnConnectedClient(object? sender, ClientSessionInfoEventArgs e)
+    {
+      if (InvokeRequired)
+      {
+        Invoke(() => OnConnectedClient(sender, e));
+        return;
+      }
+
+      var item = new ListViewItem(e.ClientSessionInfo.ToString())
+      {
+        Tag = e.ClientSessionInfo
+      };
+      _lvOnlineClients.Items.Add(item);
+    }
+
+    private void OnDisconnectedClient(object? sender, ClientSessionInfoEventArgs e)
+    {
+      if (InvokeRequired)
+      {
+        Invoke(() => OnDisconnectedClient(sender, e));
+        return;
+      }
+
+      foreach (ListViewItem item in _lvOnlineClients.Items)
+      {
+        if (item.Tag is ClientSessionInfo client)
+        {
+          _lvOnlineClients.Items.Remove(item);
+          break;
+        }
+      }
+    }
+
+    private void OnClientDoubleClick(object? sender, EventArgs e)
+    {
+      if (_lvOnlineClients.SelectedItems.Count == 0) return;
+
+      var selectedItem = _lvOnlineClients.SelectedItems[0];
+      var clientInfo = (ClientSessionInfo)selectedItem.Tag!;
+
+      // TODO: Open DM form with selected client
+      // new DirectMessageForm(_server, clientInfo).Show();
+    }
+  }
+}
