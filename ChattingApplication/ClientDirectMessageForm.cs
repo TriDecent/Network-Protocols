1b5c95459d6e779c39f4a0206e8c7686c7c64af1
TriDecent <116976929+TriDecent@users.noreply.github.com>
2025-01-27 23:51:47 +0700

    add ClientDirectMessageForm template with designer and resource files

---
+++ ChattingApplication/ClientDirectMessageForm.cs
@@ -0,0 +1,10 @@
+﻿namespace ChattingApplication
+{
+  public partial class ClientDirectMessageForm : Form
+  {
+    public ClientDirectMessageForm()
+    {
+      InitializeComponent();
+    }
+  }
+}
