c4852b23438de4a8e19b214bfc9b86b66ae2906b
TriDecent <116976929+TriDecent@users.noreply.github.com>
2025-02-05 23:21:30 +0700

    update ITcpClient with connection status and async connect method

--- ChattingApplication/Core/Models/ITcpClient.cs
+++ ChattingApplication/Core/Models/ITcpClient.cs
@@ -1,7 +1,12 @@
+using System.Net;
+
 namespace ChattingApplication.Core.Models;
 
-public interface ITcpClient
+public interface ITcpClient : IDisposable
 {
+  bool Connected { get; }
+
   Stream GetStream();
   void Close();
+  Task ConnectAsync(IPEndPoint endPoint, CancellationToken token);
 }
--- ChattingApplication/Core/Models/WrapperTcpClient.cs
+++ ChattingApplication/Core/Models/WrapperTcpClient.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Sockets;
 
 namespace ChattingApplication.Core.Models;
@@ -6,6 +7,11 @@ public class WrapperTcpClient(TcpClient tcpClient) : ITcpClient
 {
   private readonly TcpClient _client = tcpClient;
 
+  public bool Connected => _client.Connected;
+
   public Stream GetStream() => _client.GetStream();
+  public async Task ConnectAsync(IPEndPoint endPoint, CancellationToken token)
+    => await _client.ConnectAsync(endPoint, token);
   public void Close() => _client.Close();
+  public void Dispose() => _client.Dispose();
 }
